9e0d9d6a4f6b26b21d9b476ee232476f0e1436b3
jmaguire <jmaguire@alumni.stanford.edu>
2021-09-09 12:41:33 -0700

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ResponseAutomation',
+      version='1.0',
+      description='Playground to test response automation features',
+      author='Cookie Monster',
+      packages=['recommendation_engine', 'recommendation_engine.helpers'],
+     )
