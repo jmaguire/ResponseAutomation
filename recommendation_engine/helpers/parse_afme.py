90b8d8c40f479e17dcdfbcff90a6f6969ac3b714
jmaguire <jmaguire@alumni.stanford.edu>
2021-09-09 12:27:36 -0700

    Adding initial files

---
+++ recommendation_engine/helpers/parse_afme.py
@@ -0,0 +1,49 @@
+from docx import Document
+import numpy as np
+import csv
+import re
+import argparse
+
+class Parser():
+    def __init__(self, filename):
+        self.filename = filename
+        self.document = document = Document(filename)
+        self.question_numbers = []
+        self.question_text = []
+        self.question_array = []
+
+    def parse_questions(self):
+        for table in self.document.tables:
+            if len(table.rows) != 1:
+                continue
+            cell_text = table.cell(0, 0).text
+            match = bool(re.match(r'\d[\.\d\.]*\d\s', cell_text))
+            if match:
+                question_number = cell_text[:cell_text.find(' ')].strip()
+                question_text = cell_text.replace(question_number, '').strip()
+                self.question_numbers.append(question_number)
+                self.question_text.append(question_text)
+                self.question_array.append([question_number, question_text])
+
+    def save_to_csv(self, filename):
+        with open(filename, 'w') as f:
+            csv.writer(f).writerows(self.question_array)
+
+    def get_question_text(self):
+        return self.question_text
+
+    def get_question_numbers(self):
+        return self.question_numbers
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Parse afme files")
+    parser.add_argument('file', help='A required integer positional argument')
+    args = parser.parse_args()
+    print(args.file)
+    parser = Parser(args.file)
+    parser.save_to_csv('data/test.csv')
+
+
+if __name__ == "__main__":
+    main()
---
+++ recommendation_engine/recommendations.py
@@ -0,0 +1,46 @@
+import os
+import re
+from docx import Document
+import csv
+import numpy as np
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import sys
+import helpers
+
+class Recommendations():
+
+    def __init__(self, model_type = 'msmarco-distilbert-base-v4'):
+        self.model_type = model_type
+        self.model = SentenceTransformer('msmarco-distilbert-base-v4')
+
+    def build_model(self, sentences = []):
+        self.sentences = sentences
+        self.sentence_array = np.asarray(sentences)
+        self.sentence_embeddings = self.model.encode(sentences);
+
+    def print_matches(self, text, matches = 5):
+        text_embedding = self.model.encode([text])
+        scores = cosine_similarity(
+            [text_embedding[0]],
+            self.sentence_embeddings
+        )
+        scores = scores[0]
+        indices = np.argpartition(scores, -matches)[-matches:]
+        indices = indices[np.argsort(-1*scores[indices])]
+        for index in indices:
+            print(str((scores[index]*100).round(1)) + "%: " + str(self.sentence_array[index]))
+        print("\n\n")
+
+def main():
+    parser = helpers.Parser('data/afme.docx')
+    parser.parse_questions()
+    recommend = Recommendations()
+    recommend.build_model(parser.get_question_text())
+
+    while True:
+        txt = input("Enter question text: ")
+        recommend.print_matches(txt)
+
+if __name__ == "__main__":
+    main()
